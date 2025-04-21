3dadc98f2cd377d2d3ae3c31a0e5cb987fd12bd9
Jakub Popiołek <grimurin@gmail.com>
2025-04-21 22:38:34 +0200

    Init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './authentication/ui/login-page/login-page.component';
+import { RegisterPageComponent } from './authentication/ui/register-page/register-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginPageComponent,
+    },
+    {
+        path: 'register',
+        component: RegisterPageComponent,
+    }
+];
