531814d519ee4ab9b5f96c588a3b7147edb9d1ed
Jakub Popiołek <grimurin@gmail.com>
2025-06-25 17:51:16 +0200

    Add terraform and github pipeline files

---
+++ terraform/prod_infrastructure/outputs.tf
@@ -0,0 +1,9 @@
+output "static_webapp_name" {
+  value = azurerm_static_web_app.static_web_app.name
+  description = "Nazwa statycznej aplikacji webowej"  
+}
+
+output "static_webapp_url" {
+  value = azurerm_static_web_app.static_web_app.default_host_name
+  description = "URL statycznej aplikacji weboweej"
+}
---
+++ terraform/prod_infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix_ui" {
+  default = "gamegather-ui"  
+}
+
+variable "resource_group_name" {
+  default = "gamegather-rg"  
+}
+
+variable "location" {
+  default = "westeurope"
+}
